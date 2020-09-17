8c5585e0113098a18d318408ec9ca922b0789293
José Domingos <domliraa@gmail.com>
2020-09-16 23:58:13 -0300

    Add HomePage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ front-web/src/pages/HomePage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ReactComponent as ArrowIcon } from '../../assets/ArrowLeft.svg'
+import { ReactComponent as GameIcon } from '../../assets/gamer.svg'
+import './styles.css'
+
+const HomePage: React.FC = () => {
+    return (
+        <div className="home-container">
+            <div className="home-text">
+                <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
+                <h3 className="home-text-subtitle">Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!</h3>
+                <div className="home-actions">
+                    <button className="home-btn">QUERO SABER QUAIS SÃO</button>
+                    <div className="home-btn-icon">
+                        <ArrowIcon />
+                    </div>
+                </div>
+            </div>
+            <GameIcon className="home-image"/>
+        </div>
+    );
+}
+
+export default HomePage;
